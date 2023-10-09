ae563f93691471c0e9867f1f77e103167ee2776c
DanieLuan <danielluanlourencol@gmail.com>
2023-10-09 01:56:25 -0300

    initial commit

---
+++ app/src/main/java/br/imd/imd_calculadora/activities/MainActivity.java
@@ -0,0 +1,57 @@
+package br.imd.imd_calculadora.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import br.imd.imd_calculadora.R;
+import br.imd.imd_calculadora.fragments.CalculadoraFragment;
+import br.imd.imd_calculadora.fragments.NotasFragment;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonCalculadora, buttonNotas;
+    private CalculadoraFragment calculadoraFragment;
+    private NotasFragment notasFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonCalculadora = findViewById(R.id.buttonCalculadora);
+        buttonNotas = findViewById(R.id.buttonNotas);
+
+        notasFragment = new NotasFragment();
+
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.frameLayoutMain, notasFragment);
+        transaction.commit();
+
+        buttonCalculadora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculadoraFragment = new CalculadoraFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameLayoutMain, calculadoraFragment);
+                transaction.commit();
+            }
+        });
+
+        buttonNotas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameLayoutMain, notasFragment);
+                transaction.commit();
+            }
+        });
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "imd-calculadora"
+include(":app")
+ 
