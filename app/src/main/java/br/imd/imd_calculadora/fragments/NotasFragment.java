add02a81c1d5670d481975b29c3733348fab9da1
DanieLuan <danielluanlourencol@gmail.com>
2023-10-11 23:53:44 -0300

    fix: NotasFragment refactores

--- app/src/main/java/br/imd/imd_calculadora/fragments/NotasFragment.java
+++ app/src/main/java/br/imd/imd_calculadora/fragments/NotasFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import br.imd.imd_calculadora.R;
+import br.imd.imd_calculadora.util.InputFilterMinMax;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,7 +28,7 @@ public class NotasFragment extends Fragment {
     private EditText nota1;
     private EditText nota2;
     private EditText nota3;
-    private TextView situacao;
+    private TextView situation;
     private Button calcularButton;
 
     public NotasFragment() {
@@ -45,80 +48,113 @@ public class NotasFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_notas, container, false);
+
         nota1 = (EditText) view.findViewById(R.id.textNumberNota1);
         nota2 = (EditText) view.findViewById(R.id.textNumberNota2);
         nota3 = (EditText) view.findViewById(R.id.textNumberNota3);
+
         calcularButton = (Button) view.findViewById(R.id.buttonCalcularNotas);
-        situacao = (TextView) view.findViewById(R.id.textSituacao);
+        situation = (TextView) view.findViewById(R.id.textSituation);
+
+        nota1.setFilters(new InputFilter[]{new InputFilterMinMax()});
+        nota2.setFilters(new InputFilter[]{new InputFilterMinMax()});
+        nota3.setFilters(new InputFilter[]{new InputFilterMinMax()});
 
         calcularButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View viewOnClick) {
-                if(editTextLength(nota2) == 0 && editTextLength(nota3) == 0) {
-                    String situacaoAprovado, situacaoAprovadoPorNota;
-                    float numNota1 = editTextGetNumber(nota1);
-                    float notaFinalAprovado = (21 - numNota1)/2;
-                    float notaFinalAprovadoPorNota = (15 - numNota1)/2;
-                    situacaoAprovado = "Situação:\n" + "Para ser aprovado: U2: " + notaFinalAprovado + " U3: " + notaFinalAprovado;
+                if (isEditTextEmpty(nota1)){
+                    showToastMensage("AVISO: É preciso colocar pelo menos a nota da unidade 1.");
+                    return;
+                }
+
+                String SituationAprovado, SituationAprovadoPorNota;
+
+                float numNota1 = editTextGetNumber(nota1);
+                float numNota2 = editTextGetNumber(nota2);
+                float numNota3 = editTextGetNumber(nota3);
+
+
+                if(isEditTextEmpty(nota2) && isEditTextEmpty(nota3)) {
+                    //SITUAÇÃO 1: APENAS A NOTA DA UNIDADE 1
+
+                    float notaFinalAprovado = calculateGradeNoToPass(numNota1, numNota2, 21)/2;
+                    float notaFinalAprovadoPorNota = calculateGradeNoToPass(numNota1, numNota2, 15)/2;
+
+                    //VERIFICA SE PRECISA FAZER RECUPERAÇÃO POIS A NOTA É MAIOR QUE 10
+                    if(notaFinalAprovadoPorNota > 10 || notaFinalAprovado > 10){
+                        SituationAprovado = "Situação:\nPrecisa fazer recuperação.";
+                        SituationAprovadoPorNota = "";
+                        situation.setText(SituationAprovado + "\n" + SituationAprovadoPorNota);
+                        return;
+                    }
+
+                    SituationAprovado = "Situação:\n" + "Para ser aprovado: U2: " + notaFinalAprovado + " U3: " + notaFinalAprovado;
                     if(numNota1 < 3){
-                        situacaoAprovadoPorNota = "Não pode mais ser aprovado por nota.";
+                        SituationAprovadoPorNota = "Não pode mais ser aprovado por nota.";
                     } else {
-                        situacaoAprovadoPorNota = "Para ser aprovado por nota: U2: " + notaFinalAprovadoPorNota + " U3: " + notaFinalAprovadoPorNota;
+                        SituationAprovadoPorNota = "Para ser aprovado por nota: U2: " + notaFinalAprovadoPorNota + " U3: " + notaFinalAprovadoPorNota;
                     }
-                    situacao.setText(situacaoAprovado + "\n" + situacaoAprovadoPorNota);
-                } else if (editTextLength(nota3) == 0) {
-                    //TODO calculate nota3 to get approved
-                    String situacaoAprovado, situacaoAprovadoPorNota;
-                    float numNota1 = editTextGetNumber(nota1);
-                    float numNota2 = editTextGetNumber(nota2);
-                    float notaFinalAprovado = (21 - numNota1 + numNota2)/2;
-                    float notaFinalAprovadoPorNota = (15 - numNota1 + numNota2)/2;
-                    situacaoAprovado = "Situação:\n" + "Para ser aprovado: U3: " + notaFinalAprovado;
-                    if(notaFinalAprovadoPorNota > 10){
-                        situacaoAprovado = "Situação:\nPrecisa fazer recuperação.";
-                        situacaoAprovadoPorNota = "";
-                    } else if (numNota1 < 3 || numNota2 < 3 ) {
-                        situacaoAprovadoPorNota = "Não pode mais ser aprovado por nota.";
+                    situation.setText(SituationAprovado + "\n" + SituationAprovadoPorNota);
+
+                } else if (isEditTextEmpty(nota3)) {
+                    //SITUAÇÃO 2: NOTAS DAS UNIDADES 1 E 2
+
+                    float notaFinalAprovado = calculateGradeNoToPass(numNota1, numNota2, 21);
+                    float notaFinalAprovadoPorNota = calculateGradeNoToPass(numNota1, numNota2, 15);
+
+                    //VERIFICA SE PRECISA FAZER RECUPERAÇÃO POIS A NOTA É MAIOR QUE 10
+                    if(notaFinalAprovadoPorNota > 10 || notaFinalAprovado > 10){
+                        SituationAprovado = "Situação:\nPrecisa fazer recuperação.";
+                        SituationAprovadoPorNota = "";
+                        situation.setText(SituationAprovado + "\n" + SituationAprovadoPorNota);
+                        return;
+                    }
+
+                    SituationAprovado = "Situação:\n" + "Para ser aprovado: U3: " + notaFinalAprovado;
+                    if (numNota1 < 3 || numNota2 < 3 ) {
+                        SituationAprovadoPorNota = "Não pode mais ser aprovado por nota.";
                     } else {
-                        situacaoAprovadoPorNota = "Para ser aprovado por nota: U3: " + notaFinalAprovadoPorNota;
+                        SituationAprovadoPorNota = "Para ser aprovado por nota: U3: " + notaFinalAprovadoPorNota;
                     }
-                    situacao.setText(situacaoAprovado + "\n" + situacaoAprovadoPorNota);
+                    situation.setText(SituationAprovado + "\n" + SituationAprovadoPorNota);
+
                 } else if (editTextLength(nota1) != 0 && editTextLength(nota2) != 0 && editTextLength(nota3) != 0) {
-                    String situacao;
-                    float numNota1 = editTextGetNumber(nota1);
-                    float numNota2 = editTextGetNumber(nota2);
-                    float numNota3 = editTextGetNumber(nota3);
+                    String toastSituation;
+
                     float mediaFinal = Math.round((numNota1+numNota2+numNota3)/3* 2)/2;
                     if(mediaFinal >= 7) {
-                        situacao = "APROVADO com média " + mediaFinal;
+                        toastSituation = "APROVADO com média " + mediaFinal;
                     } else if (mediaFinal >= 5) {
-                        situacao = "APROVADO por nota com média " + mediaFinal;
+                        toastSituation = "APROVADO por nota com média " + mediaFinal;
                     } else {
-                        situacao = "REPROVADO com média " + mediaFinal;
+                        toastSituation = "REPROVADO com média " + mediaFinal;
                     }
-                    showToastMensage(situacao);
-                } else if (editTextLength(nota1) == 0){
-                    showToastMensage("AVISO: É preciso colocar pelo menos a nota da unidade 1.");
+                    showToastMensage(toastSituation);
                 } else {
                     showToastMensage("AVISO: Digite alguma nota.");
                 }
             }
         });
 
-
-
         return view;
     }
-
+    private float calculateGradeNoToPass(float numNota1, float numNota2, float targetTotal) {
+        return (targetTotal - (numNota1 + numNota2));
+    }
     private void showToastMensage(String text) {
         Toast.makeText(getActivity(), text, Toast.LENGTH_LONG).show();
     }
     private float editTextGetNumber(EditText ed) {
-        return Float.parseFloat(ed.getText().toString());
+        String text = ed.getText().toString();
+        return (text.isEmpty()) ? 0.0f : Float.parseFloat(text);
     }
     private int editTextLength(EditText ed) {
         return ed.getText().toString().length();
+    }
+
+    private boolean isEditTextEmpty(EditText ed){
+        return ed.getText().toString().isEmpty();
     }
 }
---
+++ app/src/main/java/br/imd/imd_calculadora/util/InputFilterMinMax.java
@@ -0,0 +1,42 @@
+package br.imd.imd_calculadora.util;
+
+import android.text.InputFilter;
+import android.text.Spanned;
+
+/**
+ * Class that implements InputFilter methods and overrides charSquence filter to a certain range MIN and MAX.
+ */
+public class InputFilterMinMax implements InputFilter {
+
+    private int MIN_RANGE = 0;
+    private int MAX_RANGE = 10;
+
+    /**
+     * An InputFilter that restricts text input to decimal values within a specific range (0 to 10).
+     *
+     *
+     * @param source String from the EditText element
+     * @param start Start position of the text
+     * @param end End position of the text
+     * @param dest Spanned elemnt to the existing text
+     * @param dstart The start position of the existing text that will be replaced
+     * @param dend The end position of the existing text that will be replaced.
+     * @return The filtered text to be accepted or an empty string if the input is outside the specified range or invalid.
+     *
+     */
+    @Override
+    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+        try {
+            String newInput = dest.subSequence(0, dstart).toString() + source + dest.subSequence(dend, dest.length());
+            double input = Double.parseDouble(newInput);
+
+            if (input >= 0 && input <= 10) {
+                return null;
+            }
+        } catch (NumberFormatException e) {
+            // A entrada não é um número válido, então não permite a entrada.
+        }
+
+        return "";
+    }
+}
